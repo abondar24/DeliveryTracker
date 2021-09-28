3574444d1fc4fcf5f2d0db13dd1a722b9caebfe5
abondar24 <abondar1992@gmail.com>
2021-09-28 10:53:49 +0200

    main class

--- DeliveryApi/src/main/java/org/abondar/experimental/delivery/api/Main.java
+++ DeliveryApi/src/main/java/org/abondar/experimental/delivery/api/Main.java
@@ -1,7 +1,22 @@
 package org.abondar.experimental.delivery.api;
 
-public class Main {
-    public static void main(String[] args) {
 
+import io.vertx.reactivex.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
+        var vertx = Vertx.vertx();
+
+        vertx.rxDeployVerticle(new DeliveryApiVerticle())
+                .subscribe(
+                        ok -> logger.info("Server up on port 8000"),
+                        err -> logger.info("Error: ",err)
+                );
     }
 }
